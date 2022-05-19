c123b6314f9276dddc91449a3be647e66f311e22
Roderick Karlemstrand <roderick@karlemstrand.com>
2022-05-19 11:29:59 +0200

    Test count all files in dir (fails)

--- tests/test_local_counter.py
+++ tests/test_local_counter.py
@@ -1,4 +1,5 @@
 import unittest
+from pathlib import Path
 
 from models.base_counter import BaseCounter
 
@@ -29,3 +30,35 @@ class TestLocalCounter(unittest.TestCase):
 
         with self.assertRaises(KeyError):
             self.assertEqual(word_frequency_dict['.'], 1)
+
+    def test_count_multiple_files(self):
+        """
+        Given path to the directory containing multiple input files,
+        When count_word_frequencies_in_directory(),
+        Then output matches the manually counted result.
+        """
+        # setup
+        data_path = '../data/test_count_multiple_files'
+        Path("./results").mkdir(parents=True, exist_ok=True)
+
+        base_counter = BaseCounter(data_path)
+
+        with open(f'{data_path}/single_line.txt', "w") as text_file:
+            text_file.write('A very very simple text.')
+
+        with open(f'{data_path}/poly_line.txt', "w") as text_file:
+            text_file.write('A very very simple text\n')
+            text_file.write('A very very simple text\r\n')
+            text_file.write('A very very simple text.')
+
+        word_frequency_dict = base_counter.count_word_frequencies_in_directory()
+
+        # testing
+        self.assertEqual(word_frequency_dict['a'], 4)
+        self.assertEqual(word_frequency_dict['very'], 8)
+        self.assertEqual(word_frequency_dict['simple'], 4)
+        self.assertEqual(word_frequency_dict['text'], 4)
+
+        # edge cases
+        with self.assertRaises(KeyError):
+            self.assertEqual(word_frequency_dict['texta'], 2)
