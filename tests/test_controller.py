236f7ebf8888982ad2134f5c7275401ca70de3e1
Roderick Karlemstrand <roderick@karlemstrand.com>
2022-05-19 16:12:03 +0200

    Add test to send a piece of text to server (fails)

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -32,3 +32,26 @@ class TestController(unittest.TestCase):
             self.assertIsNotNone(res)
             self.assertEqual(res, b'21')
             print(res)
+
+    def test_send_text(self):
+        """
+        Given one line of text,
+        When send_text() to an echoing server,
+        Then decoded received message matches the original text.
+        """
+        echo_server = EchoBaseServer('127.0.0.1', 50001)
+        t1 = Thread(target=echo_server.start)
+        t1.start()
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect(('127.0.0.1', 50001))
+
+            client = Controller(s)
+            text = 'A very very simple text.'
+
+            ok, res = client.send_text(text)
+
+            self.assertTrue(ok)
+            self.assertIsNotNone(res)
+            self.assertEqual(res, text)
+            print(res)
